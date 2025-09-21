a5a81244af6fc2a6e877ae38f1af385a875bf56e
LakshmiVvs <vvslakshmi791@gmail.com>
2025-09-22 00:56:47 +0530

    passwd hiding1

---
+++ datatypes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# every thing in shell is string
+ NUM1=100
+ NUM2=200
+TOTAL=$((NUM1+$NUM2))
+father=$
+mother=$
+brother=$
+sister=$
+
+Family=("father" , "mother" , "brother" , "sister")
+
+echo "my family memmers are ::${family[0]}"
+
+echo " head of my family :: ${family[0]}"
+
+echo " queen of my family :: ${family[1]}"
