679011b34abe676b993b6df2640dea3ba4a4974a
LakshmiVvs <vvslakshmi791@gmail.com>
2025-09-21 21:01:12 +0530

    passwd hiding1

--- variables-01.sh
+++ variables-01.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Arguments passing
 person1=$1
 person2=$2
 echo "$person1 ::hi $person2"
@@ -6,3 +7,9 @@ echo "$person2 ::hello $person1"
 echo "$person1 ::how r u"
 echo "$person2 ::good ram how about u"
 echo "$person1 ::iam fine thank u" 
+
+# password hiding
+
+echo " PLEASE ENTER PASSWORD"
+read -s enter pin_num # this is variable
+echo "PIN ENTERED: $pin_num"
